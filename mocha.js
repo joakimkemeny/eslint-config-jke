592dbe4636528c1cb5decc3a14e5208e84a5aafb
Joakim Kemeny <joakim@kemeny.se>
2016-11-06 02:50:10 +0100

    Updated dependencies and adjusted rules.

--- mocha.js
+++ mocha.js
@@ -5,12 +5,14 @@ module.exports = {
 
 		// Mocha
 		"mocha/handle-done-callback": "error",
+		"mocha/max-top-level-suites": "off",
 		"mocha/no-exclusive-tests": "error",
 		"mocha/no-global-tests": "error",
 		"mocha/no-hooks": "off",
 		"mocha/no-hooks-for-single-case": "off",
 		"mocha/no-identical-title": "off",
 		"mocha/no-mocha-arrows": "off",
+		"mocha/no-nested-tests": "error",
 		"mocha/no-pending-tests": "error",
 		"mocha/no-return-and-callback": "error",
 		"mocha/no-sibling-hooks": "error",
